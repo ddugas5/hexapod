cac1217d4dae8f2ccf852d7a6bcbf326fe34a67a
Davey <ddugashf1@gmail.com>
2025-12-27 11:11:18 -0600

    servo node can control one servo to go to a commanded position

--- src/servo_control/servo_control/servo_control_node.py
+++ src/servo_control/servo_control/servo_control_node.py
@@ -1,18 +1,30 @@
 import rclpy
-from rclpy.node import node
+from rclpy.node import Node
+from std_msgs.msg import Float32
+from adafruit_servokit import ServoKit
 
 class ServoControlNode(Node):
     def __init__(self):
         super().__init__('servo_control_node')
         self.subscription = self.create_subscription(
-            Float32Array,
-            '/ik_angles',
+            Float32,
+            '/servo_angle',
             self.angle_callback,
             10
         )
 
-    def angle_callback(self, msg):
-        pass
+        #set the number of channels on the board
+        self.kit = ServoKit(channels=16)
+
+        #set the servo channel to the channel you want
+        self.servo_channel = 0
+
+    def angle_callback(self, msg: Float32):
+        angle = msg.data    #get data and assign to variable
+
+        self.kit.servo[self.servo_channel].angle = angle #attach the specified channel to the servo and the recieved angle to the servo
+
+        self.get_logger().info(f'Set servo to angle {angle:.1f} degrees')
 
 def main(args=None):
     rclpy.init(args=args)
