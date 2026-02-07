4e8156ffffcf15c4f0f7ad7a099250450e4d24e8
Davey <ddugashf1@gmail.com>
2026-02-07 11:15:24 -0600

    Implemented translational and rotational velcoity vectors to the controller. Forward and backwards movement works well, however, the turns seem like they need a little work. Turning to the right seems off while left turns seem okay

--- src/trajectory_control/trajectory_control/foot_trajectory_controller.py
+++ src/trajectory_control/trajectory_control/foot_trajectory_controller.py
@@ -14,11 +14,26 @@ class FootTrajectoryController(Node):
             10
         )
 
+        # self.subscription = self.create_subscription(Joy, "/joy", self.joystick_callback, 10)
+
+        #leg bases relative to body center
+        self.nominal = [
+            [ 2, 3.5],   # front left
+            [ 0, 3.5],   # middle left
+            [ -2, 3.5],   # rear left
+            [ 2, 3.5],   # front right
+            [ 0, 3.5],   # middle right
+            [ -2, 3.5],   # rear right
+        ]
+
         self.gait_period = 1.0 #sec
         self.start_time = time.time()
 
         self.step_height = 2.0 #in
-        self.stride = 3.0
+        self.stride = 2.0
+
+        self.vx = 2.0 #forward velocity
+        self.wz = 0.0 #yaw rate
 
         #leg index mapping
         self.tripod_A = [0, 2, 4] #front left, rear left, middle right
@@ -43,6 +58,10 @@ class FootTrajectoryController(Node):
         self.timer = self.create_timer(
             1.0/self.rate, self.timer_callback
         )
+
+    # def joystick_callback(self):
+    #     self.vx = 
+    #     self.wz = 
 
     def timer_callback(self):
         # t = (time.time() - self.start_time) / self.duration
@@ -91,6 +110,9 @@ class FootTrajectoryController(Node):
 
         t = (time.time() - self.start_time) / self.gait_period #make global timer and phase calculation
         phase = t % 1.0 #take the fractional part of t and you will always have a phase between 0 & 1
+        body_x = self.vx * self.gait_period * phase #body movement in the x direction
+        theta = self.wz * self.gait_period * phase #body angle rotation
+
 
         targets = []
 
@@ -106,17 +128,31 @@ class FootTrajectoryController(Node):
             if leg_phase < 0.5:
                 #swing phase
                 s = leg_phase / 0.5 #normalize it to 0->1 for only the first half of the phase (s=0 is start of swing, s=1 is end of swing)
-                x = -self.stride / 2 + s * self.stride #linear interpolation for the x linear movement of the swing
+                step = self.vx * self.gait_period
+                x = -step / 2 + s * step #linear interpolation for the x linear movement of the swing
                 z = -2.0 + self.step_height * np.sin(np.pi * s) #z vertical lift in a sin wave from 0 -> pi
 
             else:
                 #stance phase
                 s = (leg_phase - 0.5) / 0.5 #again normalize 0 -> 1 for the stance phase time
-                x = self.stride / 2 - s * self.stride #linear interpolate the x linear movement for the push along the floor
+                step = self.vx * self.gait_period
+                x = step / 2 - s * step #linear interpolate the x linear movement for the push along the floor
                 z = -2.0 #z stays on the ground
 
-            y = 3.0 #per leg offsets later
-            targets.extend([x, y, z]) #append 3D position to the list
+            # y = 3.0 #per leg offsets later
+            # targets.extend([x - body_x, y, z]) #append 3D position to the list
+            bx, by = self.nominal[leg_idx]
+
+            # rotate nominal point
+            rot_x = bx * np.cos(theta) - by * np.sin(theta)
+            rot_y = bx * np.sin(theta) + by * np.cos(theta)
+
+            foot_x = rot_x + x - body_x
+            foot_y = rot_y
+            foot_z = z
+
+            targets.extend([foot_x, foot_y, foot_z])
+
 
         msg = Float32MultiArray()
         msg.data = targets
