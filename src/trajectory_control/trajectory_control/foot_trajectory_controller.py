206c6e7c4e9fdfee8c303c48a1a1bd0460176977
Davey <ddugashf1@gmail.com>
2026-02-08 14:57:59 -0600

    Integrated PS4 Controller with the ROS2 Joy Node to control the movement of the robot. Now I need to add some more hardware and do some tweaking to get the leg movements in the right places. I also need to add some software to change teh chassis height from the ground as well.

--- src/trajectory_control/trajectory_control/foot_trajectory_controller.py
+++ src/trajectory_control/trajectory_control/foot_trajectory_controller.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
+from sensor_msgs.msg import Joy
 import numpy as np
 import time
 
@@ -14,7 +15,7 @@ class FootTrajectoryController(Node):
             10
         )
 
-        # self.subscription = self.create_subscription(Joy, "/joy", self.joystick_callback, 10)
+        self.subscription = self.create_subscription(Joy, "/joy", self.joystick_callback, 10)
 
         #leg bases relative to body center
         self.nominal = [
@@ -32,8 +33,8 @@ class FootTrajectoryController(Node):
         self.step_height = 2.0 #in
         self.stride = 2.0
 
-        self.vx = 2.0 #forward velocity
-        self.wz = 0.0 #yaw rate
+        self.vx = 0.0
+        self.wz = 0.0
 
         #leg index mapping
         self.tripod_A = [0, 2, 4] #front left, rear left, middle right
@@ -59,104 +60,78 @@ class FootTrajectoryController(Node):
             1.0/self.rate, self.timer_callback
         )
 
-    # def joystick_callback(self):
-    #     self.vx = 
-    #     self.wz = 
+    def joystick_callback(self, msg):
+        self.right_joystick = msg.axes[4]
+        self.left_joystick = msg.axes[0]
+
+        #scale for increasing speed
+        self.vx = self.right_joystick * 3.0
+        self.wz = self.left_joystick
+
+        # self.get_logger().info(f"vx={self.vx:.2f}, wz={self.wz:.2f}")
 
     def timer_callback(self):
-        # t = (time.time() - self.start_time) / self.duration
-        # t = np.clip(t, 0.0, 1.0)
+        if abs(self.vx) < 0.01 and abs(self.wz) < 0.01:
+            #set all legs to standing position
+            targets=[]
+            for leg_idx in range(6):
+                bx, by = self.nominal[leg_idx]
+                rot_x = bx * np.cos(0) - by * np.sin(0)
+                rot_y = bx * np.sin(0) + by * np.cos(0)
+                foot_x = rot_x
+                foot_y = rot_y
+                foot_z = -2.0
+                targets.extend([foot_x, foot_y, foot_z])
 
-        # if self.phase == "swing":
-        #     #loop through arrays of "start" and "end" and linear interpolate x, y, and z linearly
-        #     pos = self.swing_start + t * (self.swing_end - self.swing_start)
+        else:
 
-        #     #modify the 3rd value in the array to make the z go linear but have a sinusoidal lift
-        #     pos[2] += self.step_height * np.sin(np.pi * t)
+            t = (time.time() - self.start_time) / self.gait_period #make global timer and phase calculation
+            phase = t % 1.0 #take the fractional part of t and you will always have a phase between 0 & 1
+            body_x = self.vx * self.gait_period * phase #body movement in the x direction
+            theta = self.wz * self.gait_period * phase #body angle rotation
 
-        # elif self.phase == "stance":
-        #     pos = self.stance_start + t * (self.stance_end - self.stance_start)
-        #     #no vertical lift here, this is where the leg moves back to original stance
+            targets = []
+            #loop through all six legs in the index
+            for leg_idx in range(6):
+                
+                leg_phase = phase #set the leg phase to what was calculated above
+                #for legs in tripod B, add 0.5 to shift the phase by half (halfway through the phase is where you want 3/6 legs to be)
+                if leg_idx in self.tripod_B:
+                    leg_phase = (phase + 0.5) % 1.0 
 
-        #publish array as a point
-        # msg = Float32MultiArray()
-        # x = pos[0]
-        # y = pos[1]
-        # z = pos[2]
+                #condition is first half of the leg cycle
+                if leg_phase < 0.5:
+                    #swing phase
+                    s = leg_phase / 0.5 #normalize it to 0->1 for only the first half of the phase (s=0 is start of swing, s=1 is end of swing)
+                    step = self.vx * self.gait_period
+                    x = -step / 2 + s * step #linear interpolation for the x linear movement of the swing
+                    z = -2.0 + self.step_height * np.sin(np.pi * s) #z vertical lift in a sin wave from 0 -> pi
 
-        # msg.data = [
-        #     0.0, 3.0, -2.0,
-        #     0.0, 3.0, -2.0,
-        #     0.0, 3.0, -2.0,
-        #     0.0, 3.0, -2.0,
-        #     0.0, 3.0, -2.0,
-        #     0.0, 3.0, -2.0,
-        # ]
+                else:
+                    #stance phase
+                    s = (leg_phase - 0.5) / 0.5 #again normalize 0 -> 1 for the stance phase time
+                    step = self.vx * self.gait_period
+                    x = step / 2 - s * step #linear interpolate the x linear movement for the push along the floor
+                    z = -2.0 #z stays on the ground
 
-        # msg.data[3] = x
-        # msg.data[4] = y
-        # msg.data[5] = z
+                # y = 3.0 #per leg offsets later
+                # targets.extend([x - body_x, y, z]) #append 3D position to the list
+                bx, by = self.nominal[leg_idx]
 
-        # self.goal_publisher.publish(msg)
+                # rotate nominal point
+                rot_x = bx * np.cos(theta) - by * np.sin(theta)
+                rot_y = bx * np.sin(theta) + by * np.cos(theta)
 
-        # #start timer over to make it repeat on loop
-        # if t >= 1.0:
-        #     self.start_time= time.time()
+                foot_x = rot_x + x - body_x
+                foot_y = rot_y
+                foot_z = z
 
-        #     if self.phase == "swing":
-        #         self.phase = "stance"
-        #     else:
-        #         self.phase = "swing"
-
-        t = (time.time() - self.start_time) / self.gait_period #make global timer and phase calculation
-        phase = t % 1.0 #take the fractional part of t and you will always have a phase between 0 & 1
-        body_x = self.vx * self.gait_period * phase #body movement in the x direction
-        theta = self.wz * self.gait_period * phase #body angle rotation
-
-
-        targets = []
-
-        #loop through all six legs in the index
-        for leg_idx in range(6):
-            
-            leg_phase = phase #set the leg phase to what was calculated above
-            #for legs in tripod B, add 0.5 to shift the phase by half (halfway through the phase is where you want 3/6 legs to be)
-            if leg_idx in self.tripod_B:
-                leg_phase = (phase + 0.5) % 1.0 
-
-            #condition is first half of the leg cycle
-            if leg_phase < 0.5:
-                #swing phase
-                s = leg_phase / 0.5 #normalize it to 0->1 for only the first half of the phase (s=0 is start of swing, s=1 is end of swing)
-                step = self.vx * self.gait_period
-                x = -step / 2 + s * step #linear interpolation for the x linear movement of the swing
-                z = -2.0 + self.step_height * np.sin(np.pi * s) #z vertical lift in a sin wave from 0 -> pi
-
-            else:
-                #stance phase
-                s = (leg_phase - 0.5) / 0.5 #again normalize 0 -> 1 for the stance phase time
-                step = self.vx * self.gait_period
-                x = step / 2 - s * step #linear interpolate the x linear movement for the push along the floor
-                z = -2.0 #z stays on the ground
-
-            # y = 3.0 #per leg offsets later
-            # targets.extend([x - body_x, y, z]) #append 3D position to the list
-            bx, by = self.nominal[leg_idx]
-
-            # rotate nominal point
-            rot_x = bx * np.cos(theta) - by * np.sin(theta)
-            rot_y = bx * np.sin(theta) + by * np.cos(theta)
-
-            foot_x = rot_x + x - body_x
-            foot_y = rot_y
-            foot_z = z
-
-            targets.extend([foot_x, foot_y, foot_z])
+                targets.extend([foot_x, foot_y, foot_z])
 
 
-        msg = Float32MultiArray()
-        msg.data = targets
-        self.goal_publisher.publish(msg)
+            msg = Float32MultiArray()
+            msg.data = targets
+            self.goal_publisher.publish(msg)
 
 def main():
     rclpy.init()
